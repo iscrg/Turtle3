b4d47703295bcf9d1b0ef07f905394ba2ea3877e
Ivan <95573815+frosesss@users.noreply.github.com>
2023-03-02 11:14:42 +0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,35 @@
+'''
+Title: eto rossiya!!!
+Group:
+Popov I.
+Fedyakin D.
+Fisher D.
+'''
+
+def triangle(x, y, a, b):
+    '''
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :return:
+    '''
+    pass
+
+def draw_dog():
+    pass
+
+def draw_cat():
+    pass
+
+def main():
+    '''
+    Main function
+    :return: None
+    '''
+
+    draw_cat()
+    draw_dog()
+
+if __name__ == '__main__':
+    main()
