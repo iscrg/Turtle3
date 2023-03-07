d3748e57c99d609019df784fc85c5685711f2e2f
Danchik452 <fisherd2004@gmail.com>
2023-03-08 00:14:00 +0700

    ellipse

--- main.py
+++ main.py
@@ -26,17 +26,27 @@ def ellipse(x, y, a, b, angle, fill_color, border_color, border_width):
     '''
     responsible person: Fisher D.
 
-    :param x:
-    :param y:
-    :param a:
-    :param b:
-    :param angle:
+    :param x: X start coordinate
+    :param y: Y start coordinate
+    :param a: ellipse lenght
+    :param b: ellipse width
+    :param angle: angle of inclination
     :param fill_color: Fill color
     :param border_color: Border color
     :border_width: Border width
     :return: None
     '''
-    pass
+    import turtle
+    import math
+
+    turtle.rt(angle)
+    turtle.color(border_color, fill_color)
+    turtle.pensize(border_width)
+    for degree in range(361):
+        rad = math.radians(degree)
+        x = a * math.sin(rad) + x
+        y = -b * math.cos(rad) + b + y
+        turtle.goto(x, y)
 
 def rectangle(x, y, a, b, angle, fill_color, border_color, border_width):
     '''
@@ -62,3 +72,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+ellipse(0,0,50,100,45,"green","yellow",5)
